da7f9393179bf0753863cf9e4be7f2835a4ebd5c
Tom Jenkinson <tom.jenkinson@soundcloud.com>
2018-04-26 09:50:41 +0200

    be smarter calculating what index to continue from when loop items change

--- src/loop.js
+++ src/loop.js
@@ -1,14 +1,22 @@
 import { toDomEl } from './helpers.js';
+import longestSubstring from 'longest-common-substring';
 
 export function loop(marquee, buildersIn=[], seperatorBuilder=null) {
   let lastIndex = -1;
   let builders = buildersIn.slice();
+
+  const getNextBuilder = (offset = 1) => {
+    const nextIndex = (lastIndex+offset) % builders.length;
+    return { builder: builders[nextIndex], index: nextIndex };
+  }
+
   const appendItem = (spaceJustAvailable) => {
     if (!builders.length || !marquee.isWaitingForItem()) {
       return;
     }
-    lastIndex = (lastIndex+1) % builders.length;
-    let $item = toDomEl(builders[lastIndex]());
+    const { builder, index } = getNextBuilder();
+    lastIndex = index;
+    let $item = toDomEl(builder());
     if (spaceJustAvailable && seperatorBuilder) {
       const $seperator = toDomEl(seperatorBuilder());
       const $container = document.createElement('div');
@@ -24,6 +32,29 @@ export function loop(marquee, buildersIn=[], seperatorBuilder=null) {
   appendItem();
   return {
     update: (newBuilders) => {
+
+      // try and start from somewhere that makes sense
+      const calculateNewIndex = () => {
+        // convert array of function references to array of ids
+        const buildersStructure = builders.map((b, i) => {
+          const prevIndex = builders.indexOf(b);
+          // if already seen builder, give it the same number
+          return prevIndex < i ? prevIndex : i;
+        });
+        const newBuildersStructure = newBuilders.map((b, i) => {
+          // matching indexes where they exist, and -1 for all unknown
+          return builders.indexOf(b);
+        });
+
+        const { startString1, startString2, length } = longestSubstring(buildersStructure, newBuildersStructure);
+        if (lastIndex >= startString1 && lastIndex < startString1 + length) {
+          // we are in the overlapping region
+          return lastIndex + (startString2 - startString1);
+        }
+        return -1;
+      };
+
+      lastIndex = calculateNewIndex();
       builders = newBuilders.slice();
       appendItem(false);
     }
