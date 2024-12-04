e0e95fcf5626a52a59dada4abd4a083dd506bd41
Tom Jenkinson <tom@tjenkinson.me>
2024-12-04 22:11:40 +0000

    wip

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -12,22 +12,32 @@ const $marquee = document.getElementById('marquee')!;
 
 const marquee = (window.m = new Marquee($marquee, {
   rate: -100,
+  // rate: -500,
 }));
 
-window.l = loop(
-  marquee,
-  [
-    () => 'Contrary to popular belief, Lorem Ipsum is not simply random text',
-    () =>
-      'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
-    () =>
-      'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source',
-    () =>
-      'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC',
-  ],
-  () => {
-    const $separator = document.createElement('div');
-    $separator.innerHTML = '&nbsp•&nbsp';
-    return $separator;
-  },
+// window.l = loop(
+//   marquee,
+//   [
+//     () =>
+//       'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'.repeat(
+//         1,
+//       ),
+//     // () =>
+//     //   'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
+//     // () =>
+//     //   'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source',
+//     // () =>
+//     //   'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC',
+//   ],
+//   () => {
+//     const $separator = document.createElement('div');
+//     $separator.innerHTML = '&nbsp•&nbsp';
+//     return $separator;
+//   },
+// );
+
+marquee.appendItem(
+  'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'.repeat(
+    1,
+  ),
 );
