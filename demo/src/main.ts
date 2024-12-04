9179c575e49c2a7a9104bc0ace8273c07b60f63b
Tom Jenkinson <tom@tjenkinson.me>
2024-12-04 22:16:07 +0000

    Update main.ts

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -15,27 +15,6 @@ const marquee = (window.m = new Marquee($marquee, {
   // rate: -500,
 }));
 
-// window.l = loop(
-//   marquee,
-//   [
-//     () =>
-//       'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'.repeat(
-//         1,
-//       ),
-//     // () =>
-//     //   'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',
-//     // () =>
-//     //   'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source',
-//     // () =>
-//     //   'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC',
-//   ],
-//   () => {
-//     const $separator = document.createElement('div');
-//     $separator.innerHTML = '&nbsp•&nbsp';
-//     return $separator;
-//   },
-// );
-
 marquee.appendItem(
   'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'.repeat(
     1,
